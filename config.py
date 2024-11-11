47231680d3327beccad877fbfac63f1405cc23b0
Elizabeth <elizabethhelget94@gmail.com>
2024-11-11 11:41:37 -0600

    v4 - updates to UI and added AI functionality

---
+++ config.py
@@ -0,0 +1,18 @@
+# config.py
+
+# Path to OCI config file
+OCI_CONFIG_PATH = "C:/Users/EB1801445/.oci/config"  # Replace with actual path
+
+# OCI config profile
+OCI_CONFIG_PROFILE = "DEFAULT"
+
+# Generative AI Model ID
+GENERATIVE_AI_MODEL_ID = "ocid1.generativeaimodel.oc1.us-chicago-1.amaaaaaask7dceya7ozidbukxwtun4ocm4ngco2jukoaht5mygpgr6gq2lgq"
+
+# Service endpoint
+SERVICE_ENDPOINT = "https://inference.generativeai.us-chicago-1.oci.oraclecloud.com"
+
+# Object Storage Configuration
+NAMESPACE = "axl92nk10tnu"                # Correct namespace
+BUCKET_NAME = "bucket-20241109-2154"      # Correct bucket name
+COMPARTMENT_NAME = "elizabethhelget94"    # Correct compartment name
